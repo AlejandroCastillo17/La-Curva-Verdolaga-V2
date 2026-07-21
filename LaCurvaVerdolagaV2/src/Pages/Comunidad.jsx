8eec990f8c26cfbe8489afb0ef78184666e4f573
JuanTB300405 <127706094+JuanTB300405@users.noreply.github.com>
2026-07-21 11:07:41 -0500

    avances primera parte de la pagina de comunidad

--- LaCurvaVerdolagaV2/src/Pages/Comunidad.jsx
+++ LaCurvaVerdolagaV2/src/Pages/Comunidad.jsx
@@ -13,7 +13,7 @@ const Comunidad = () => {
         <section className="comunidad_contenido">
           <div className="contenido_foro">
             <div className="foro_header">
-              <h1>FORO DE INCHAS</h1>
+              <h1>FORO DE HINCHAS</h1>
               <div className="header_icon">
                 <button>Crear Publicacion</button>
                 <svg
@@ -30,6 +30,153 @@ const Comunidad = () => {
                   <path d="M12 5l0 14" />
                   <path d="M5 12l14 0" />
                 </svg>
+              </div>
+            </div>
+            <div className="foro_publicaciones">
+              <div className="publicaciones_publicacion">
+                <div className="publicaciones_img1"></div>
+                <div className="publicaciones_contenido">
+                  <div className="contenido_etiquetas">
+                    <span className="etiquetas_etiqueta">HISTORIA</span>
+                    <div>
+                      <span>HACE</span> <span>2</span> <span>HORAS</span>
+                    </div>
+                  </div>
+                  <div className="etiquetas_info">
+                    <h1>
+                      El día que conquistamos el continente: Memorias de 1989
+                    </h1>
+                    <p>
+                      Revivimos paso a paso la gesta heroica del equipo de Pacho
+                      Maturana contra Olimpia. Un relato desde la tribuna del
+                      ...
+                    </p>
+                  </div>
+                  <div className="info_user">
+                    <div className="info_user_icon">
+                      <span>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-width="1"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        >
+                          <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                          <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                        </svg>
+                      </span>
+
+                      <p>Jose maria cordoba</p>
+                    </div>
+                    <div className="user_options">
+                      <div className="options_option">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-width="1"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        >
+                          <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+                        </svg>
+                        <span>1.2k</span>
+                      </div>
+                      <div className="options_option">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                        >
+                          <path d="M5.821 4.91c3.899 -2.765 9.468 -2.539 13.073 .535c3.667 3.129 4.168 8.238 1.152 11.898c-2.841 3.447 -7.965 4.583 -12.231 2.805l-.233 -.101l-4.374 .931l-.04 .006l-.035 .007h-.018l-.022 .005h-.038l-.033 .004l-.021 -.001l-.023 .001l-.033 -.003h-.035l-.022 -.004l-.022 -.002l-.035 -.007l-.034 -.005l-.016 -.004l-.024 -.005l-.049 -.016l-.024 -.005l-.011 -.005l-.022 -.007l-.045 -.02l-.03 -.012l-.011 -.006l-.014 -.006l-.031 -.018l-.045 -.024l-.016 -.011l-.037 -.026l-.04 -.027l-.002 -.004l-.013 -.009l-.043 -.04l-.025 -.02l-.006 -.007l-.056 -.062l-.013 -.014l-.011 -.014l-.039 -.056l-.014 -.019l-.005 -.01l-.042 -.073l-.007 -.012l-.004 -.008l-.007 -.012l-.014 -.038l-.02 -.042l-.004 -.016l-.004 -.01l-.017 -.061l-.007 -.018l-.002 -.015l-.005 -.019l-.005 -.033l-.008 -.042l-.002 -.031l-.003 -.01v-.016l-.004 -.054l.001 -.036l.001 -.023l.002 -.053l.004 -.025v-.019l.008 -.035l.005 -.034l.005 -.02l.004 -.02l.018 -.06l.003 -.013l1.15 -3.45l-.022 -.037c-2.21 -3.747 -1.209 -8.391 2.413 -11.119z" />
+                        </svg>
+
+                        <span>84</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="publicaciones_publicacion">
+                <div className="publicaciones_img2"></div>
+                <div className="publicaciones_contenido">
+                  <div className="contenido_etiquetas">
+                    <span className="etiquetas_etiqueta">ANÁLISIS</span>
+                    <div>
+                      <span>HACE</span> <span>5</span> <span>HORAS</span>
+                    </div>
+                  </div>
+                  <div className="etiquetas_info">
+                    <h1>Tactics Corner: El nuevo rol del mediocampo</h1>
+                    <p>
+                      ¿Cómo ha cambiado el esquema táctico en los últimos
+                      partidos? Analizamos las estadísticas de posesión y ...
+                    </p>
+                  </div>
+                  <div className="info_user">
+                    <div className="info_user_icon">
+                      <span>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-width="1"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        >
+                          <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                          <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                        </svg>
+                      </span>
+
+                      <p>felipe cadavid restrepo</p>
+                    </div>
+                    <div className="user_options">
+                      <div className="options_option">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-width="1"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        >
+                          <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+                        </svg>
+                        <span>456</span>
+                      </div>
+                      <div className="options_option">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                        >
+                          <path d="M5.821 4.91c3.899 -2.765 9.468 -2.539 13.073 .535c3.667 3.129 4.168 8.238 1.152 11.898c-2.841 3.447 -7.965 4.583 -12.231 2.805l-.233 -.101l-4.374 .931l-.04 .006l-.035 .007h-.018l-.022 .005h-.038l-.033 .004l-.021 -.001l-.023 .001l-.033 -.003h-.035l-.022 -.004l-.022 -.002l-.035 -.007l-.034 -.005l-.016 -.004l-.024 -.005l-.049 -.016l-.024 -.005l-.011 -.005l-.022 -.007l-.045 -.02l-.03 -.012l-.011 -.006l-.014 -.006l-.031 -.018l-.045 -.024l-.016 -.011l-.037 -.026l-.04 -.027l-.002 -.004l-.013 -.009l-.043 -.04l-.025 -.02l-.006 -.007l-.056 -.062l-.013 -.014l-.011 -.014l-.039 -.056l-.014 -.019l-.005 -.01l-.042 -.073l-.007 -.012l-.004 -.008l-.007 -.012l-.014 -.038l-.02 -.042l-.004 -.016l-.004 -.01l-.017 -.061l-.007 -.018l-.002 -.015l-.005 -.019l-.005 -.033l-.008 -.042l-.002 -.031l-.003 -.01v-.016l-.004 -.054l.001 -.036l.001 -.023l.002 -.053l.004 -.025v-.019l.008 -.035l.005 -.034l.005 -.02l.004 -.02l.018 -.06l.003 -.013l1.15 -3.45l-.022 -.037c-2.21 -3.747 -1.209 -8.391 2.413 -11.119z" />
+                        </svg>
+
+                        <span>32</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
